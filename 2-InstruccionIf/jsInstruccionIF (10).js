b49b7ece20191d6b7b36b4d0ee5a467835fa3cf3
bertora132 <36174646+bertora132@users.noreply.github.com>
2018-02-15 22:15:53 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,22 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+var nota;
 
+nota = Math.floor (Math.random() * 10 ) + 1;
+
+if( nota <4 )
+{
+	alert( nota + " Vamos, la próxima se puede");
+}
+
+else if (nota <= 8)
+{
+	alert( nota + " APROBÓ");
+}
+
+else
+{
+	alert( nota + " Exelente");
+}
 }//FIN DE LA FUNCIÓN
