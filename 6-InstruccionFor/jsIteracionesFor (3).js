afeae6d8df40fa310f1ceeb7c90bbbfec44665e3
bertora132 <36174646+bertora132@users.noreply.github.com>
2018-02-22 22:16:51 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -2,7 +2,11 @@ function Mostrar()
 {
 
 var repetciones = prompt("ingrese el número de repeticiones");
+ 
 
-
+for ( var numrep = repetciones ; repetciones > 1 ; repetciones ++)
+{
+    console.log("Hola UTN FRA");
+}
 
 }//FIN DE LA FUNCIÓN
--- 6-InstruccionFor/jsIteracionesFor (5).js
+++ 6-InstruccionFor/jsIteracionesFor (5).js
@@ -3,5 +3,22 @@ function Mostrar()
 
 
 
+for ( ;  ;  )         
+{
+    var num = parseInt(prompt("Ingrese un numero"));
+   
+   
+    if( num == 9 )
+    {
+        break;
+
+    }
+
+
+}
+
+alert(" Ingresaste el 9");
+
+
 
 }//FIN DE LA FUNCIÓN
